500e3324bab3676743437fcd854526e232a3f298
Yahir Leo <yahirrocki@outlook.com>
2023-03-24 12:31:36 -0600

    yea

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = import('express');
+const path = import('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
